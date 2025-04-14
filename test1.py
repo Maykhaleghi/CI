edb9d670d2868e16fd39aeaa1a53f668492e6ea1
maykhaleghi <maykhaleghi@gmail.com>
2025-04-14 20:42:46 +0330

    hi again

--- test1.py
+++ test1.py
@@ -1,1 +1,12 @@
-print("hello")
+# algorithms/pso_algorithm.py
+from algorithms.base_algorithm import BaseAlgorithm
+
+class PSOAlgorithm(BaseAlgorithm):
+    def __init__(self, num_particles, c1, c2):
+        self.num_particles = num_particles
+        self.c1 = c1
+        self.c2 = c2
+
+    def run(self):
+        print("Running Particle Swarm Optimization...")
+        # Implementation of PSO logic here
